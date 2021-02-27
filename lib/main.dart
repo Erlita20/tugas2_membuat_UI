118007f98b535a50c4096b8c6488806d1e0ddd96
= <=>
2021-02-27 10:17:37 +0700

    tugas2 ui

--- lib/main.dart
+++ lib/main.dart
@@ -9,105 +9,153 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text("MyApp"), backgroundColor: Colors.red),
+        body: Column(
+          children: [
+            Container(
+              alignment: Alignment.topCenter,
+              child: Text(
+                'BERITA TERBARU     PERTANDINGAN HARI INI',
+                style: TextStyle(fontSize: 15, height: 3.0),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              margin: EdgeInsets.only(top: 12),
+              decoration: BoxDecoration(
+                border: Border(
+                  left: BorderSide(
+                    color: Colors.purpleAccent,
+                  ),
+                  top: BorderSide(
+                    color: Colors.purpleAccent,
+                  ),
+                  right: BorderSide(
+                    color: Colors.purpleAccent,
+                  ),
+                ),
+              ),
+              child: Image(
+                image: NetworkImage(
+                    'https://asset.kompas.com/crops/3xTK4p6NP3PqdGM8mE_4Ay1PyRQ=/247x0:958x474/750x500/data/photo/2019/07/27/5d3ba98ca67ed.jpg'),
+              ),
+            ),
+            Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    left: BorderSide(
+                      color: Colors.purpleAccent,
+                    ),
+                    right: BorderSide(
+                      color: Colors.purpleAccent,
+                    ),
+                  ),
+                ),
+                margin: const EdgeInsets.all(0.0),
+                padding: const EdgeInsets.all(10.0),
+                alignment: Alignment.topCenter,
+                child: Text(
+                  'Costa Mendekat Ke Palmeiras',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(3),
+              color: Colors.purpleAccent,
+              alignment: Alignment.centerLeft,
+              child: Text("Transfer", style: TextStyle(fontSize: 14)),
+              height: 45.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+            ),
+            Container(
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.blueGrey)),
+              child: Column(
+                children: <Widget>[
+                  new Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Expanded(
+                        child: Image.network(
+                            'https://img.inews.co.id/media/822/files/inews_new/2020/04/08/gerard_pique.jpg'),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                          child: Center(
+                        child: Text(
+                            "Pique Bilang Wait Untungkan Madrid, Koeman Tepuk Jidat",
+                            style: TextStyle(fontSize: 12)),
+                      )),
+                      SizedBox(
+                        width: 10,
+                      ),
+                    ],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8.0),
+                    alignment: Alignment.centerLeft,
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Colors.blueGrey.withOpacity(0.6))),
+                    child: Text("Barcelona Feb 13,2021",
+                        style: TextStyle(fontSize: 12)),
+                    height: 40,
+                    width: 500,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.blueGrey)),
+                    child: Column(children: <Widget>[
+                      new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Expanded(
+                            child: Image.network(
+                                'https://img.inews.co.id/media/822/files/inews_new/2020/04/08/gerard_pique.jpg'),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                              child: Center(
+                            child: Text(
+                                "Pique Bilang Wait Untungkan Madrid, Koeman Tepuk Jidat",
+                                style: TextStyle(fontSize: 12)),
+                          )),
+                          SizedBox(
+                            width: 10,
+                          ),
+                        ],
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(8.0),
+                        alignment: Alignment.centerLeft,
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Colors.blueGrey.withOpacity(0.6))),
+                        child: Text("Barcelona Feb 13,2021",
+                            style: TextStyle(fontSize: 12)),
+                        height: 40,
+                        width: 500,
+                      ),
+                    ]),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
